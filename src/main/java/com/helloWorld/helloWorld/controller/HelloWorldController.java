161edb185ce61d8cc84170a00b21f35a708b8658
Gabriel Camargo <camargoxg@gmail.com>
2024-07-01 23:35:35 -0300

    Primeiras práticas Spring Boot

---
+++ src/main/java/com/helloWorld/helloWorld/controller/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.helloWorld.helloWorld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exemplos")
+public class HelloWorldController {
+	@GetMapping("/helloworld")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+	
+	@GetMapping("/bsm")
+	public String listaBSMs() {
+		return "Orientação ao futuro \nPersistência \nResponsabilidade pessoal \nOrientação ao detalhe \nTrabalho em equipe \nProatividade \nComunicação \nMentalidade de crescimento";
+	}
+	
+	@GetMapping("/objetivos")
+	public String listaObjetivos() {
+		return "Primeiros passos com Spring Boot \nSessão de Mentoria \nSessão de Empregabilidade";
+	}
+ }
